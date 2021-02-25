88f4549225a541992f16d2df1ad06e6c17cf2f60
Eric Della <ericdella13@gmail.com>
2021-02-24 19:20:47 -0800

    GET account by ID works

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -12,14 +12,16 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-// router.get('/:id', (req, res, next) => {
-//   try{
+router.get('/:id', async  (req, res, next) => {
+  try{
 
-//   }
-//   catch(err){
-//     next(err)
-//   }
-// })
+    const [account] = await accounts.getById(req.params.id)
+    res.json(account)
+  }
+  catch(err){
+    next(err)
+  }
+})
 
 // router.post('/', (req, res, next) => {
 //   try{
